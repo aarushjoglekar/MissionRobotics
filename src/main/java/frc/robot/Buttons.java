d66bc8f604fab719688946cd9c96555386378a7b
ClawlessTiger <aarush.joglekar@gmail.com>
2025-10-15 19:21:57 -0700

    buttons

--- src/main/java/frc/robot/Buttons.java
+++ src/main/java/frc/robot/Buttons.java
@@ -6,27 +6,27 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 public class Buttons {
     private static final Joystick joystick = new Joystick(0);
 
-    private static int A = 1;
-    private static int B = 2;
-    private static int X = 3;
-    private static int Y = 4;
+    private static int Z = 1;
+    private static int X = 2;
+    private static int C = 3;
+    private static int V = 4;
     private static int LEFT_BUMPER = 5;
     private static int RIGHT_BUMPER = 6;
-    private static int LEFT_STICK_Y = 1;
-    private static int RIGHT_STICK_X = 0; // 2 for joystick, 0 for keyboard
+    private static int FORWARD_BACK_W_S = 1;
+    private static int LEFT_RIGHT_A_D = 0;
 
-    public static JoystickButton joystickAButton = new JoystickButton(joystick, B);
-    public static JoystickButton joystickBButton = new JoystickButton(joystick, A);
-    public static JoystickButton joystickXButton = new JoystickButton(joystick, Y);
-    public static JoystickButton joystickYButton = new JoystickButton(joystick, X);
+    public static JoystickButton keyboardZButton = new JoystickButton(joystick, Z);
+    public static JoystickButton keyboardXButton = new JoystickButton(joystick, X);
+    public static JoystickButton keyboardCButton = new JoystickButton(joystick, C);
+    public static JoystickButton keybordVButton = new JoystickButton(joystick, V);
     public static JoystickButton joystickLeftBumperButton = new JoystickButton(joystick, LEFT_BUMPER);
     public static JoystickButton joystickRightBumperButton = new JoystickButton(joystick, RIGHT_BUMPER);
 
     public static double getLeftStickY() {
-        return -joystick.getRawAxis(LEFT_STICK_Y);
+        return -joystick.getRawAxis(FORWARD_BACK_W_S);
     }
     
     public static double getRightStickX() {
-        return -joystick.getRawAxis(RIGHT_STICK_X);
+        return -joystick.getRawAxis(LEFT_RIGHT_A_D);
     }
 }
