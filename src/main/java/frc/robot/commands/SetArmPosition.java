ba7d7d6e5333dd83ea178278f1ad0aeeca662dda
RpresstoPasta <lonmow02@gmail.com>
2025-10-18 21:33:02 +0000

    prestonc

--- src/main/java/frc/robot/commands/SetArmPosition.java
+++ src/main/java/frc/robot/commands/SetArmPosition.java
@@ -1,4 +1,30 @@
 package frc.robot.commands;
 
-public class SetArmPosition {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Arm.ArmPosition;
+
+public class SetArmPosition extends Command{
+Arm arm = Arm.getInstance(): 
+ArmPosition pos;
+
+    public SetArmPosition (ArmPosition pos) {
+        this.pos = pos;x
+    }
+
+    @Override
+    public void initialize() {
+        arm.setPos(ArmPosition.UP);
+        super.initialize();
+    }
+@Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        super.execute();
+    }j[]
+    @Override
+    public boolean isFinished() {
+        // TODO Auto-generated method stub
+        return super.isFinished();
+    }
 }
