f498f3c0976f084207bd0b2d6327d07d0071399c
Uzma375411 <uzmamohammad375411@gmail.com>
2025-10-18 21:33:46 +0000

    commands

--- src/main/java/frc/robot/commands/linefollowing/LineFollowing.java
+++ src/main/java/frc/robot/commands/linefollowing/LineFollowing.java
@@ -1,4 +1,52 @@
 package frc.robot.commands.linefollowing;
 
-public class LineFollowing {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.ReflectiveSensor;
+
+public class LineFollowing extends Command{
+    ReflectiveSensor sensor = ReflectiveSensor.getInstance(); 
+    Drivetrain drive = Drivetrain.getInstance(); 
+    public boolean rightDetects() {
+        return sensor.rightValue() < 0.7;
+    }
+
+    public boolean leftDetects() {
+        return sensor.leftValue() < 0.7;
+    }
+
+    public LineFollowing() {
+
+    }
+    
+    @Override
+    public void initialize() {
+        // TODO Auto-generated method stub
+        super.initialize();
+    }
+
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        super.execute();
+        if (rightDetects() && leftDetects()) {
+            drive.arcadeDrive(1, 0);
+        } else if (rightDetects()){
+            drive.arcadeDrive(1, 1);
+        } else if (leftDetects()){
+            drive.arcadeDrive(1, -1);
+        }
+    }
+
+
+    @Override
+    public boolean isFinished() {
+        // TODO Auto-generated method stub
+        return (!leftDetects() && !rightDetects());
+    }
+    @Override
+    public void end(boolean interrupted) {
+        // TODO Auto-generated method stub
+        super.end(interrupted);
+    }
 }
