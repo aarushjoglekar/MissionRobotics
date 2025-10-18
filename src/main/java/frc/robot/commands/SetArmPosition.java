e592c6d4ca1b287b722b6b7a8510ecea80e01547
turquoise25799 <turquoise25799@gmail.com>
2025-10-18 21:33:16 +0000

    commands

--- src/main/java/frc/robot/commands/SetArmPosition.java
+++ src/main/java/frc/robot/commands/SetArmPosition.java
@@ -1,4 +1,34 @@
 package frc.robot.commands;
 
-public class SetArmPosition {
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Arm.ArmPosition;
+
+public class SetArmPosition extends Command{
+    Arm arm = Arm.getInstance();
+    ArmPosition pos;
+    public SetArmPosition(ArmPosition pos) {
+        this.pos=pos;
+    }
+
+    @Override
+    public void initialize() {
+        arm.setPos(ArmPosition.UP);
+    }
+    @Override
+    public void execute() {
+        // TODO Auto-generated method stub
+        super.execute();
+    }
+    @Override
+    public boolean isFinished() {
+        // TODO Auto-generated method stub
+        return super.isFinished();
+    }
+@Override
+public void end(boolean interrupted) {
+    // TODO Auto-generated method stub
+    super.end(interrupted);
+}
+
 }
